1049a27ac1be0341f64024fc965d136926db31cc
allan <moulin.allan@zoho.eu>
2019-04-12 19:53:25 +0200

    Allan debut service angular

--- ProjetAngular/src/app/Service/client.service.ts
+++ ProjetAngular/src/app/Service/client.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Client } from '../Model/client';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  url="http://localhost:8081/AppSystemeAgence/client/"
+  constructor(private http:HttpClient) { }
+
+  //methode ajouter client
+  public ajouterClient(clIn:Client):Observable<Client>{
+    return this.http.post<Client>(`${this.url}ajoutCli`,clIn);
+  }
+
+  //methode modifier client
+  public modifierClient(clIn:Client):Observable<Client>{
+    return this.http.put<Client>(`${this.url}modifCli`,clIn);
+  }
+
+  //methode afficher liste des biens a louer
+  public afficherTousBaL(){
+    return this.http.get(`${this.url}listeBaL`);
+  }
+
+  //methode rechercher un bien a louer par son id
+  public chercherBaLParId(id:number){
+    return this.http.get(`${this.url}recBaL?pId=${id}`);
+  }
+
+  //methode afficher biens a vendre
 }
