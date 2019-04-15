2383f3f7d8f6f1e06e0bbfba5cdad4594325b103
allan <moulin.allan@zoho.eu>
2019-04-15 11:29:02 +0200

    allan creation des component visite

---
+++ ProjetAngular/src/app/ajouter-visite/ajouter-visite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajouter-visite',
+  templateUrl: './ajouter-visite.component.html',
+  styleUrls: ['./ajouter-visite.component.css']
+})
+export class AjouterVisiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ProjetAngular/src/app/supprimer-visite/supprimer-visite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supprimer-visite',
+  templateUrl: './supprimer-visite.component.html',
+  styleUrls: ['./supprimer-visite.component.css']
+})
+export class SupprimerVisiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
