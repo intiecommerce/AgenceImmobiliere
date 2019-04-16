9189d5d5ce5eef02f656f55d61d052eabccc32b7
joachim napias <joachim.napias@gmail.com>
2019-04-16 14:41:22 +0200

    jogg: modif visite

--- ProjetAngular/src/app/Model/visite.ts
+++ ProjetAngular/src/app/Model/visite.ts
@@ -1,11 +1,12 @@
 import { Personne } from './personne';
 import { BiensImmo } from './biens-immo';
+import { Time } from '@angular/common';
 
 export class Visite {
 
     public id:number;
     public dateVisite:Date;
-    public heureVisite:Date;
+    public heureVisite:Time;
     public client:Personne;
     public conseillerVisite:Personne;
     public bienImmo:BiensImmo;
