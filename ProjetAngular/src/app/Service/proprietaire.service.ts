f4005feb3f386862eedbc0e5e9c11979a878a5a4
Tryobobo <gbonnenf@gmail.com>
2019-04-16 12:23:29 +0200

    bobo polopopopo

--- ProjetAngular/src/app/Service/proprietaire.service.ts
+++ ProjetAngular/src/app/Service/proprietaire.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Proprietaire } from '../Model/proprietaire';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProprietaireService {
 
-  constructor() { }
+  url = "http://localhost:8080/AppSystemeAgence/rest/proprietaire/"
+  constructor(private http: HttpClient) { }
+
+  //methode modifier client
+  public modifierProprietaire(propIn: Proprietaire): Observable<Proprietaire> {
+    return this.http.put<Proprietaire>(`${this.url}modifCli`, propIn);
+  }
+
+  //methode rechercher client
+  public chercherPropriotaireParId(id: number) {
+    return this.http.get(`${this.url}recProp?pId=${id}`);
+  }
+
+
 }
